8af6fc8cfd0d8f3d097a0c6d525a106d07d69d20
Dejar <dejar@spotv.net>
2023-10-23 14:27:13 +0900

    예제 처리

--- app/src/main/java/com/suein1209/sample/MainActivity.kt
+++ app/src/main/java/com/suein1209/sample/MainActivity.kt
@@ -2,10 +2,15 @@ package com.suein1209.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.suein1209.common.kotlinsupportlib.ext.isNotNullEmpty
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tt = ""
+        tt.isNotNullEmpty() // 테스트 추가
+
     }
 }
